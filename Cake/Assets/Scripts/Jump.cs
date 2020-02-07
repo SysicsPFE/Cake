b1d28c4cfaa4fcfc1407008be4af17d6c02db4be
eyamegdiche <34196676+eyamegdiche@users.noreply.github.com>
2020-02-07 21:49:48 +0100

    1ere etape

---
+++ Cake/Assets/Scripts/Jump.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpSpeed = 20f;
+    public bool inGround = true;
+    
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionStay()
+    {
+        inGround = true;
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * Time.deltaTime * jumpSpeed;
+        float vertical = Input.GetAxis("Vertical") * Time.deltaTime * jumpSpeed;
+
+        if (Input.GetKeyDown("space") && inGround)
+        {
+            rb.AddForce(new Vector3(0f,2f,0f)* jumpSpeed, ForceMode.Impulse);
+            inGround = false;
+        }
+    }
+}
---
+++ Cake/Assets/Scripts/TouchControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControl : MonoBehaviour
+{
+   
+    Material m;
+     //public string currentColor;
+     public GameObject player;
+     
+    
+
+    void Start()
+    {
+        m = GetComponent<Renderer>().material;
+        
+    }
+
+   
+    void OnMouseDown()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("mcha");
+            player.GetComponent<Renderer>().material = m; }
+    }
+   }
+
